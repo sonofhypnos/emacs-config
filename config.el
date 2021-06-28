9067a2885e163611354231d0308500687e38cc75
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-06-28 06:34:27 +0200

    Disable org-noter config

--- config.el
+++ config.el
@@ -440,61 +440,61 @@
            :unnarrowed t))))
 
 
-;;(use-package! org-noter
-;;    :after org
-;;    :config (setq org-noter-default-notes-file-names '("org-noter.org")
-;;                  org-noter-notes-search-path '(org-directory)
-;;                  org-noter-separate-notes-from-heading t))
-(use-package org-noter
-  :after (:any org pdf-view)
-  :config
-  (setq
-   ;; The WM can handle splits
-   org-noter-notes-window-location 'other-frame
-   ;; Please stop opening frames
-   org-noter-always-create-frame nil
-   ;; I want to see the whole file
-   org-noter-hide-other nil
-   ;; Everything is relative to the main notes file
-   org-noter-notes-search-path (list org-directory)
-   )
-  (require 'org-noter-pdftools))
-
-(use-package org-pdftools
-  :hook (org-mode . org-pdftools-setup-link))
-
-(use-package org-noter-pdftools
-  :after org-noter
-  :config
-  ;; Add a function to ensure precise note is inserted
-  (defun org-noter-pdftools-insert-precise-note (&optional toggle-no-questions)
-    (interactive "P")
-    (org-noter--with-valid-session
-     (let ((org-noter-insert-note-no-questions (if toggle-no-questions
-                                                   (not org-noter-insert-note-no-questions)
-                                                 org-noter-insert-note-no-questions))
-           (org-pdftools-use-isearch-link t)
-           (org-pdftools-use-freestyle-annot t))
-       (org-noter-insert-note (org-noter--get-precise-info)))))
-
-  ;; fix https://github.com/weirdNox/org-noter/pull/93/commits/f8349ae7575e599f375de1be6be2d0d5de4e6cbf
-  (defun org-noter-set-start-location (&optional arg)
-    "When opening a session with this document, go to the current location.
-With a prefix ARG, remove start location."
-    (interactive "P")
-    (org-noter--with-valid-session
-     (let ((inhibit-read-only t)
-           (ast (org-noter--parse-root))
-           (location (org-noter--doc-approx-location (when (called-interactively-p 'any) 'interactive))))
-       (with-current-buffer (org-noter--session-notes-buffer session)
-         (org-with-wide-buffer
-          (goto-char (org-element-property :begin ast))
-          (if arg
-              (org-entry-delete nil org-noter-property-note-location)
-            (org-entry-put nil org-noter-property-note-location
-                           (org-noter--pretty-print-location location))))))))
-  (with-eval-after-load 'pdf-annot
-    (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
+;;;;(use-package! org-noter
+;;;;    :after org
+;;;;    :config (setq org-noter-default-notes-file-names '("org-noter.org")
+;;;;                  org-noter-notes-search-path '(org-directory)
+;;;;                  org-noter-separate-notes-from-heading t))
+;;(use-package org-noter
+;;  :after (:any org pdf-view)
+;;  :config
+;;  (setq
+;;   ;; The WM can handle splits
+;;   org-noter-notes-window-location 'other-frame
+;;   ;; Please stop opening frames
+;;   org-noter-always-create-frame nil
+;;   ;; I want to see the whole file
+;;   org-noter-hide-other nil
+;;   ;; Everything is relative to the main notes file
+;;   org-noter-notes-search-path (list org-directory)
+;;   )
+;;  (require 'org-noter-pdftools))
+;;
+;;(use-package org-pdftools
+;;  :hook (org-mode . org-pdftools-setup-link))
+;;
+;;(use-package org-noter-pdftools
+;;  :after org-noter
+;;  :config
+;;  ;; Add a function to ensure precise note is inserted
+;;  (defun org-noter-pdftools-insert-precise-note (&optional toggle-no-questions)
+;;    (interactive "P")
+;;    (org-noter--with-valid-session
+;;     (let ((org-noter-insert-note-no-questions (if toggle-no-questions
+;;                                                   (not org-noter-insert-note-no-questions)
+;;                                                 org-noter-insert-note-no-questions))
+;;           (org-pdftools-use-isearch-link t)
+;;           (org-pdftools-use-freestyle-annot t))
+;;       (org-noter-insert-note (org-noter--get-precise-info)))))
+;;
+;;  ;; fix https://github.com/weirdNox/org-noter/pull/93/commits/f8349ae7575e599f375de1be6be2d0d5de4e6cbf
+;;  (defun org-noter-set-start-location (&optional arg)
+;;    "When opening a session with this document, go to the current location.
+;;With a prefix ARG, remove start location."
+;;    (interactive "P")
+;;    (org-noter--with-valid-session
+;;     (let ((inhibit-read-only t)
+;;           (ast (org-noter--parse-root))
+;;           (location (org-noter--doc-approx-location (when (called-interactively-p 'any) 'interactive))))
+;;       (with-current-buffer (org-noter--session-notes-buffer session)
+;;         (org-with-wide-buffer
+;;          (goto-char (org-element-property :begin ast))
+;;          (if arg
+;;              (org-entry-delete nil org-noter-property-note-location)
+;;            (org-entry-put nil org-noter-property-note-location
+;;                           (org-noter--pretty-print-location location))))))))
+;;  (with-eval-after-load 'pdf-annot
+;;    (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
 
 
 
