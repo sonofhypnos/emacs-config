ad3fb92e02e3b68692f26076491c358bf16d64a4
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-08-18 17:28:50 +0200

    Fix org-modules in init.el
    Apparently (featurep! org +roam) evaluates to false, if you accidentally
    write (org + roam). Took me a long time to debug this.

--- init.el
+++ init.el
@@ -147,7 +147,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org + deft + noter + latex-mode-hook + roam)               ; organize your plain life in plain text
+       (org +deft +noter +latex-mode-hook +roam2)               ; organize your plain life in plain text
        pdf-tools
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
