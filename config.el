2734ef39eb8a12e985bba8eef92c4bb04e7f37a5
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2021-11-21 23:35:59 +0100

    Fix keywords

--- config.el
+++ config.el
@@ -69,6 +69,54 @@
   ;; Initialize
   (anki-editor-reset-cloze-number))
 
+(after! org
+  (with-no-warnings
+  (custom-declare-face '+org-todo-cancel  '((t (:inherit (bold error org-todo)))) "") ;; see dooms org module for more examples of how to do this.
+  (custom-declare-face '+org-todo-project '((t (:inherit (bold font-lock-doc-face org-todo)))) ""))
+
+  (with-no-warnings
+    (custom-declare-face '+org-todo-active  '((t (:inherit (bold font-lock-constant-face org-todo)))) "")
+    (custom-declare-face '+org-todo-project '((t (:inherit (bold font-lock-doc-face org-todo)))) "")
+    (custom-declare-face '+org-todo-onhold  '((t (:inherit (bold warning org-todo)))) "")
+    (custom-declare-face '+org-todo-cancel  '((t (:inherit (bold error org-todo)))) ""))
+  (setq org-todo-keywords
+        '((sequence   ; Not sure what the sequence is doing here (where it gets evaluated?)
+           "TODO(t)"  ; A task that needs doing & is ready to do
+           "PROJ(P)"  ; A project, which usually contains other tasks
+           "LOOP(r)"  ; A recurring task
+           "STRT(s)"  ; A task that is in progress
+           "WAIT(w)"  ; Something external is holding up this task
+           "HOLD(h)"  ; This task is paused/on hold because of me
+           "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+           "|"
+           "DONE(d)"  ; Task successfully completed
+           "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
+          (sequence
+           "[ ](T)"   ; A task that needs doing
+           "[-](S)"   ; Task is in progress
+           "[?](W)"   ; Task is being held up or paused
+           "[??](C)"  ; Confusion marker in notes
+           "PRO(p)"  ; Pro in pro-con list
+           "CON(C)"  ; Con in pro and con list
+           "|"
+           "[X](D)")  ; Task was completed
+          (sequence
+           "|"
+           "OKAY(o)"
+           "YES(y)"
+           "NO(n)"))
+        org-todo-keyword-faces
+        '(("[-]"  . +org-todo-active)
+          ("STRT" . +org-todo-active)
+          ("[?]"  . +org-todo-onhold)
+          ("WAIT" . +org-todo-onhold)
+          ("HOLD" . +org-todo-onhold)
+          ("PRO" . +org-todo-onhold)
+          ("CON" . +org-todo-cancel)
+          ("PROJ" . +org-todo-project)
+          ("NO"   . +org-todo-cancel)
+          ("KILL" . +org-todo-cancel))))
+
 (map! (:after org-roam
         :map org-mode-map
         :localleader
@@ -390,13 +438,6 @@ With a prefix ARG, remove start location."
 (after! org
   (add-hook 'org-mode-hook #'tassilo/enable-post-tangle))
 (add-to-list 'display-buffer-alist '("*Async Shell Command*" display-buffer-no-window (nil)))
-
-(after! org-roam
-(custom-declare-face '+org-todo-cancel  '((t (:inherit (bold error org-todo)))) "") ;; see dooms org module for more examples of how to do this.
-(setq org-todo-kewords (append org-todo-keywords '(sequence "[??](l)"))
-  org-todo-keyword-faces (append org-todo-keyword-faces '(("[??]" . +org-todo-cancel))))
-(setq org-todo-kewords (append org-todo-keywords '(sequence "[!!!](j)"))
-  org-todo-keyword-faces (append org-todo-keyword-faces '(("[!!!]" . +org-todo-cancel)))))
 
 (after! emacs-lisp-mode
   (setq doom-scratch-initial-major-mode emacs-lisp-mode))
