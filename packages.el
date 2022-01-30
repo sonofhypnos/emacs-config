d9869ae1d96342a1ec6053e8e8e90ce6b82e9a35
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-01-30 19:47:35 +0100

    Add highlighting packages
    Trying this out for now

--- packages.el
+++ packages.el
@@ -67,7 +67,11 @@
 (package! i3
   :recipe (:host github
            :repo "vava/i3-emacs"))
+(package! org-remark
+  :recipe (:host github
+           :repo "nobiot/org-remark"))
 (package! emr)
+(package! highlight)
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
