bc5ed4ea222b8fd3dfe1b95794eccb9e522e484e
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-06-09 16:55:40 +0200

    Fix spell-fu and dap-mode-map

--- config.el
+++ config.el
@@ -780,7 +780,8 @@
 ;;   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\emacs\\'")
 ;;   )
 
-;; (remove-hook 'text-mode-hook #'spell-fu-mode) (not sure this was ever needed?)
+;;HACK make not everything red because of spell-fu
+(remove-hook 'text-mode-hook #'spell-fu-mode)
 
 ;;(defun my/monkeytype-mode-hook ()
 ;;  "Hooks for monkeytype-mode."
@@ -918,36 +919,38 @@
 
 (after! dap-mode
   (setq dap-python-debugger 'debugpy)
-(map! :map dap-mode-map
-      :leader
-      :prefix ("d" . "dap")
-      ;; basics
-      :desc "dap next"          "n" #'dap-next
-      :desc "dap step in"       "i" #'dap-step-in
-      :desc "dap step out"      "o" #'dap-step-out
-      :desc "dap continue"      "c" #'dap-continue
-      :desc "dap hydra"         "h" #'dap-hydra
-      :desc "dap debug restart" "r" #'dap-debug-restart
-      :desc "dap debug"         "s" #'dap-debug
+  ;; ; FIXME disabled because of bug with mode-map
 
-      ;; debug
-      :prefix ("dd" . "Debug")
-      :desc "dap debug recent"  "r" #'dap-debug-recent
-      :desc "dap debug last"    "l" #'dap-debug-last
+;; (map! :map dap-mode-map
+;;       :leader
+;;       :prefix ("d" . "dap")
+;;       ;; basics
+;;       :desc "dap next"          "n" #'dap-next
+;;       :desc "dap step in"       "i" #'dap-step-in
+;;       :desc "dap step out"      "o" #'dap-step-out
+;;       :desc "dap continue"      "c" #'dap-continue
+;;       :desc "dap hydra"         "h" #'dap-hydra
+;;       :desc "dap debug restart" "r" #'dap-debug-restart
+;;       :desc "dap debug"         "s" #'dap-debug
 
-      ;; eval
-      :prefix ("de" . "Eval")
-      :desc "eval"                "e" #'dap-eval
-      :desc "eval region"         "r" #'dap-eval-region
-      :desc "eval thing at point" "s" #'dap-eval-thing-at-point
-      :desc "add expression"      "a" #'dap-ui-expressions-add
-      :desc "remove expression"   "d" #'dap-ui-expressions-remove
+;;       ;; debug
+;;       :prefix ("dd" . "Debug")
+;;       :desc "dap debug recent"  "r" #'dap-debug-recent
+;;       :desc "dap debug last"    "l" #'dap-debug-last
 
-      :prefix ("db" . "Breakpoint")
-      :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
-      :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
-      :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
-      :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message)
+;;       ;; eval
+;;       :prefix ("de" . "Eval")
+;;       :desc "eval"                "e" #'dap-eval
+;;       :desc "eval region"         "r" #'dap-eval-region
+;;       :desc "eval thing at point" "s" #'dap-eval-thing-at-point
+;;       :desc "add expression"      "a" #'dap-ui-expressions-add
+;;       :desc "remove expression"   "d" #'dap-ui-expressions-remove
+
+;;       :prefix ("db" . "Breakpoint")
+;;       :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
+;;       :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
+;;       :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
+;;       :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message)
 
   )
 ;; the stuff below should probably be loaded later
