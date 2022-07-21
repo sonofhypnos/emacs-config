43fc51b5df475b8ed9f40abe168518d22774d3ea
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-07-21 15:10:14 +0200

    Update init.el

--- init.el
+++ init.el
@@ -145,18 +145,15 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
-        +deft
-        +noter
         +latex-mode-hook
         +roam2
         +jupyter
-        +pomodoro
-        +pdf-tools)               ; organize your plain life in plain text
+        +pomodoro)               ; organize your plain life in plain text
 
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python +conda)           ; beautiful is better than ugly
        ;;qt               ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
